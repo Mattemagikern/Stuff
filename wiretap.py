cf7dd56de4847c428ff3ddfdd72aecf9d60e5957
M <mattemagikern@gmail.com>
2018-08-21 17:32:22 +0200

    A UART logging display tool
    To easily see different loggs they are displayed in diffrent colors.
    
    Signed-off-by: M <mattemagikern@gmail.com>

---
+++ wiretap.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+from termcolor import colored
+import serial
+import time
+import sys
+import glob
+import os
+from threading import Thread
+
+def read_uart(s):
+    while s.isOpen():
+        try:
+            line = s.readline()[:-1]
+            if len(line) > 0:
+                if any(word in line for word in keywords):
+                    line = colored(line, "yellow", attrs=["bold", "underline"])
+                elif "/E" in line:
+                    line = colored(line, "red", attrs=["bold", "underline"])
+                elif "/D" in line:
+                    line = colored(line, "light_gray")
+                elif "/I" in line:
+                    line = colored(line, "cyan")
+                else:
+                    line = colored(line, "white")
+
+                print line
+        except (KeyboardInterrupt, SystemExit):
+            sys.exit(0)
+            print
+        except:
+            raise
+
+def send_input(s):
+    while s.isOpen():
+        try:
+            read = sys.stdin.readline();
+            if read:
+                s.write(read)
+                print read,
+        except (KeyboardInterrupt, SystemExit):
+            print
+            sys.exit(0)
+        except:
+            raise 
+
+if __name__ == "__main__":
+    keywords = sys.argv[1:]
+    ports = ["\\.\COM%s" % (i + 1) for i in range(10)]
+    ports += glob.glob("/dev/tty[A-Za-z]*")
+    s = None
+    for port in ports:
+        try:
+            s = serial.Serial(port, 115200, timeout = 2,
+                    bytesize = serial.EIGHTBITS,
+                    parity = serial.PARITY_NONE)
+            break
+        except Exception as _:
+            pass
+    if s:
+        os.system('clear')
+        t1 = Thread(target = read_uart, args = (s,)).start()
+        t2 = Thread(target = send_input, args = (s,)).start()
+        t1.daemon = True
+        t2.daemon = True
+        try:
+            while True:
+                sleep(1)
+        except (KeyboardInterrupt, SystemExit):
+            print
+            sys.exit(0)
+        except:
+            raise
+    else:
+        print colored("Did not find an open port :(", "red")
